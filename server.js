640e52ea871778a27668baa2d47ef97bc5129c23
ASMazini <andersonsmazini@gmail.com>
2026-04-05 20:40:48 -0300

    Versao final com detalhes e ordenacao

--- server.js
+++ server.js
@@ -8,23 +8,36 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Sua URL exata da imagem, com o banco de dados "tarefas" adicionado antes do ?
 const URL_BANCO = 'mongodb+srv://andersonsmazini_db_user:kjnPS3wVNynrzvKA@cluster0.3zoxbgb.mongodb.net/tarefas?appName=Cluster0';
 
-// Conectando ao MongoDB Atlas
 mongoose.connect(URL_BANCO)
   .then(() => console.log('MongoDB Atlas conectado com sucesso!'))
   .catch(err => console.log('Erro ao conectar no banco:', err));
 
-// Rotas do Aplicativo (Criar, Ler, Atualizar, Deletar)
+// --- ROTAS ---
+
 app.post('/tarefas', async(req, res) => { 
     const t = await Tarefa.create(req.body); 
     res.json(t);
 });
 
+// EXERCÍCIO 4: LISTAR ORDENADO (do mais novo para o mais antigo)
 app.get('/tarefas', async(req, res) => { 
-    const t = await Tarefa.find(); 
+    // .sort({ _id: -1 }) é um truque do Mongo: como o ID contém a data,
+    // colocar -1 faz os últimos criados aparecerem primeiro.
+    const t = await Tarefa.find().sort({ _id: -1 }); 
     res.json(t);
+});
+
+// EXERCÍCIO 3: BUSCAR POR ID (Para a tela de detalhes parar de carregar)
+app.get('/tarefas/:id', async(req, res) => { 
+    try {
+        const t = await Tarefa.findById(req.params.id); 
+        if (!t) return res.status(404).json({ erro: "Não encontrado" });
+        res.json(t);
+    } catch (err) {
+        res.status(400).json({ erro: "ID Inválido" });
+    }
 });
 
 app.put('/tarefas/:id', async(req, res) => { 
