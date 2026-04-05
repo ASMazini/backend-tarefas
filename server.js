a6fbf67a87dc2d38631bc82f979252aaf8a0e895
ASMazini <andersonsmazini@gmail.com>
2026-04-05 15:47:11 -0300

    Adicionando dependencias e ajuste de porta

--- server.js
+++ server.js
@@ -37,4 +37,5 @@ app.delete('/tarefas/:id', async(req, res) => {
     res.json({ok:true});
 });
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
